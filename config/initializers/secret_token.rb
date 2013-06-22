8b40a5a87e3851a1be66d5b12588c2fc9fd28b88
Chandni Mueller <chandni.mueller@gmail.com>
2013-06-22 12:20:19 +0100

    create scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = '892346d8debc375e3166545c29c29b608b23d7c71d12f83d01b11b74a4ea03634a75d1d9e8be7a88bedf3199375eb25867dcaa85aad88b69cf73ed06e3fa54c7'
